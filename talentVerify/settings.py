be8dce072f014112bfd2f35c68c23c55b4ee589d
tozy10 <141628635+tozy10@users.noreply.github.com>
2024-07-12 11:52:23 +0200

    fourth commit

--- talentVerify/settings.py
+++ talentVerify/settings.py
@@ -89,12 +89,9 @@ WSGI_APPLICATION = 'talentVerify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "db.sqlite3",
+        
 
     }
 }
